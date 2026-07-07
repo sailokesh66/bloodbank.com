e05686fd5f8583047f5debc472dd021a6271dd40
sailokesh66 <m.sslokesh6@gmail.com>
2026-07-07 18:04:12 +0530

    Fix Render port binding

--- server.py
+++ server.py
@@ -524,5 +524,12 @@ def deletenoti(id):
 
 
 
-if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=5000)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
